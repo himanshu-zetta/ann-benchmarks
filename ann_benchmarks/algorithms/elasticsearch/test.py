f89d19c2407a168e9ceb75425842382e34f63acc
himanshu <himanshu@zettabolt.com>
2025-01-29 11:04:30 +0530

    updated the requirements

---
+++ ann_benchmarks/algorithms/elasticsearch/test.py
@@ -0,0 +1,21 @@
+sample_book = {
+  "author_name": "Kate L. Mary",
+  "description_embedding": [
+      -0.24338197708129883,
+      0.08498962223529816
+  ],
+  "model_id": "sentence-transformers__msmarco-minilm-l-12-v3",
+  "book_title": "Shattered World",
+  "genres": [
+    "Horror (Zombies)", 
+    "Apocalyptic (Post Apocalyptic)", 
+    "Science Fiction",
+    "Adventure (Survival)", "Fantasy"
+  ],
+  "rating_votes": 1667,
+  "book_description": "Stranded in the middle of the Mojave Desert, surrounded by zombies, Vivian and Axl’s group are sure they’re facing the end...",
+  "review_number": 146,
+  "rating_score": 4.16,
+  "url": "https://www.goodreads.com/book/show/22693753-shattered-world",
+  "year_published": 2014
+}
